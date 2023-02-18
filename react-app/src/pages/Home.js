3836be439d349e8e396d0df570ac51720c62ed01
Akash <akkusp@outlook.com>
2023-02-18 17:22:04 -0500

    changed file name of button.css to styles.css

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { Button } from "../components/Button";
-import "../styles/Button.css";
+import { Link } from "react-router-dom";
+import "../styles/styles.css";
 
 export const Home = () => {
   return (
