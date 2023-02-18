e5a0a5bca21a3f41a8eaf6fbbaa1cb188012a952
Akash <akkusp@outlook.com>
2023-02-18 12:41:11 -0500

    fixed file structure

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,13 @@
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
