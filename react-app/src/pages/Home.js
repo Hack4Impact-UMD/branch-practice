dcd1dbde73c6a90a3060d12483c78a58659b68ca
Akash <akkusp@outlook.com>
2023-02-19 16:00:32 -0500

    reconfigured how the color changed, it would previously route to another page, however now, it simply changes the style of the current page.

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/styles.css";
+import React, { useState } from "react";
+import "../styles/Home.css";
 
 export const Home = () => {
+  const [style, changeStyle] = useState("home1");
+  const changeColor = () => {
+    if (style === "home1") {
+      changeStyle("home2");
+    } else if (style === "home2") {
+      changeStyle("home3");
+    } else {
+      changeStyle("home1");
+    }
+  };
+
   return (
-    <div className="home1">
-      <Link to={"/Home2"} className={"button"}>
+    <div className={style}>
+      <button onClick={changeColor} className={"button"}>
         Click Me!
-      </Link>
+      </button>
     </div>
   );
 };
